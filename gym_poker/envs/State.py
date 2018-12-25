ec6cb1fb39a3d84a05e1ec97e9286c78324a05c1
Tegra Morgan <tegra.morgan.net@gmail.com>
2018-12-25 15:34:14 +0200

    Encoder and Decoder are finished and tested
    Signed-off-by: Tegra Morgan <tegra.morgan.net@gmail.com>

--- gym_poker/envs/State.py
+++ gym_poker/envs/State.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-from Entities import Card
 from Entities import Hand
 
 
@@ -18,58 +17,5 @@ class State:
         # self.encoded = str(sorted(hand.cards)) + str(small_blind)
 
 
-# testing the sorting
 if __name__ == "__main__":
-    aceOfHeart = Card.Card("Heart", 14)
-    aceOfSpade = Card.Card("Spade", 14)
-    # twoOfSpade = Card.Card("Spade", 2)
-    # sevenOfSpade = Card.Card("Spade", 7)
-    # sixOfSpade = Card.Card("Spade", 6)
-    # fiveOfSpade = Card.Card("Spade", 5)
-    # print(sorted([fiveOfSpade, sixOfSpade, aceOfSpade, aceOfHeart, twoOfSpade, sevenOfSpade]))
-    # state1 = State(hand1, True)
-    # print(state1.encoded)
-    from Entities import Deck
-
-    dictionary_based_q_table = {}
-
-    deck = Deck.get_deck()
-    deck2 = Deck.get_deck()
-    codes = []
-    for small_blind in [True, False]:
-        for card1 in deck:
-            for card2 in deck2:
-                if card1 == card2:
-                    continue
-                # if card1.rank < card2.rank:
-                #     continue
-                hand = Hand.Hand()
-                hand.add_card(card1)
-                hand.add_card(card2)
-                state = State(hand, small_blind)
-                code = state.encoded
-                if code not in codes:
-                    codes.append(code)
-                    dictionary_based_q_table[code] = (0, 0)
-
-    import sys
-
-    print("size dict " + str(sys.getsizeof(dictionary_based_q_table)))
-    table_based_q_table = np.zeros([52 * 52 * 2, 2])
-
-    print("size np " + str(sys.getsizeof(table_based_q_table)))
-
-    codes.sort()
-    print(codes)
-    print(codes.__len__())
-    print(set(codes).__len__())
-
-    # print(sorted(deck, key=lambda i: i.encode()))
-    #
-    # hand = Hand.Hand()
-    # hand.add_card(aceOfHeart)
-    # hand.add_card(aceOfSpade)
-
-    #
-    # state = State(hand1, True)
-    # print(state.tupled)
+    pass
--- gym_poker/envs/poker_env.py
+++ gym_poker/envs/poker_env.py
@@ -1,21 +1,28 @@
 import gym
 from gym import spaces
 
-from Entities import Card
+from Entities.Card import Card
 from Entities.Hand import Hand
+
+SMALL_BLIND_COEFFICIENT = 52 * 53
+CHIPS_COEFFICIENT = SMALL_BLIND_COEFFICIENT * 2
 
 
 class PokerEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self):
-        self.observation_space = spaces.Tuple([spaces.Discrete(52), spaces.Discrete(52), spaces.Discrete(2)])
-        self.action_space = spaces.Discrete(2)
-        pass
-
-    def _step(self, action):
         """
+        Observation Space includes:
+        Card1, Card2, BB/SB, Chips Amount [0 -- n/2 -- n -- 1.5n -- 2n]
+          52 ,   52 ,   2  ,     4
+        """
+        self.observation_space = spaces.Tuple(
+            [spaces.Discrete(52), spaces.Discrete(52), spaces.Discrete(2), spaces.Discrete(4)])
+        self.action_space = spaces.Discrete(2)
 
+    def step(self, action):
+        """
         Parameters
         ----------
         action :
@@ -23,32 +30,31 @@ class PokerEnv(gym.Env):
         Returns
         -------
         ob, reward, episode_over, info : tuple
-            ob (object) :
-                an environment-specific object representing your observation of
-                the environment.
-            reward (float) :
-                amount of reward achieved by the previous action. The scale
-                varies between environments, but the goal is always to increase
-                your total reward.
-            episode_over (bool) :
-                whether it's time to reset the environment again. Most (but not
-                all) tasks are divided up into well-defined episodes, and done
-                being True indicates the episode has terminated. (For example,
-                perhaps the pole tipped too far, or you lost your last life.)
-            info (dict) :
-                 diagnostic information useful for debugging. It can sometimes
-                 be useful for learning (for example, it might contain the raw
-                 probabilities behind the environment's last state change).
-                 However, official evaluations of your agent are not allowed to
-                 use this for learning.
-        """
-        pass
-        # return ob, reward, episode_over, info
-
-    def _reset(self):
+        ob (object) :
+            an environment-specific object representing your observation of
+            the environment.
+        reward (float) :
+            amount of reward achieved by the previous action. The scale
+            varies between environments, but the goal is always to increase
+            your total reward.
+        episode_over (bool) :
+            whether it's time to reset the environment again. Most (but not
+            all) tasks are divided up into well-defined episodes, and done
+            being True indicates the episode has terminated. (For example,
+            perhaps the pole tipped too far, or you lost your last life.)
+        info (dict) :
+             diagnostic information useful for debugging. It can sometimes
+             be useful for learning (for example, it might contain the raw
+             probabilities behind the environment's last state change).
+             However, official evaluations of your agent are not allowed to
+             use this for learning.
+         """
         pass
 
-    def _render(self, mode='human', close=False):
+    def reset(self):
+        pass
+
+    def render(self, mode='human'):
         pass
 
     def _take_action(self, action):
@@ -57,35 +63,75 @@ class PokerEnv(gym.Env):
     def _get_reward(self):
         pass
 
-    def encode(self, hand, small_blind):
-        sorted_hand = sorted(hand.cards, reverse=True)
-        coef_blind = 52 * 52 if small_blind else 0
-        self.encoded = coef_blind + sorted_hand[0].encode() * 52 + sorted_hand[
-            1].encode()
-        # self.encoded = str(sorted(hand.cards)) + str(small_blind)
+    @staticmethod
+    def encode(hand, small_blind, _num_of_chips, initial_num_of_chips):
+        """Encoding and decoding code was lifted from openAI taxi gym"""
+        # Sort hand and extract cards
+        _sorted_hand = sorted(hand.cards, reverse=True)
+        _card1 = _sorted_hand[0].encode()
+        _card2 = _sorted_hand[1].encode()
+        # Calculate coefficient of small blind or big blind
+        _coef_blind = 1 if small_blind else 0
+        # Calculate coefficient of number of chips
+        if _num_of_chips < initial_num_of_chips:
+            _coef_chips = 0 if _num_of_chips < (initial_num_of_chips / 2) else 1
+        else:
+            _coef_chips = 2 if _num_of_chips < (initial_num_of_chips * 1.5) else 3
+        # Encode
+        encoded = _card2
+        encoded *= 52
+        encoded += _card1
+        encoded *= 2
+        encoded += _coef_blind
+        encoded *= 4
+        encoded += _coef_chips
+        return encoded
 
     @staticmethod
-    def decode(code):
-        small_blind = False
-        hand = Hand()
-        if code > 52 * 52:
-            small_blind = True
-            code -= 52 * 52
-        # encode = 52*code1 + encode
-        # % 52
-        card2 = Card.Card.decode(code % 52)
-        code = code - (code % 52)
-        card1 = Card.Card.decode(int(code / 52))
-        hand.add_card(card1)
-        hand.add_card(card2)
-        return small_blind, hand
+    def decode(_code):
+        """Encoding and decoding code was lifted from openAI taxi gym"""
+        _out = [_code % 4]
+        _code = _code // 4
+        _out.append(_code % 2)
+        _code = _code // 2
+        _card1 = Card.decode(_code % 52)
+        _code = _code // 52
+        _card2 = Card.decode(_code)
+        assert 0 <= _code < 52
+        _hand = Hand()
+        _hand.add_card(_card1)
+        _hand.add_card(_card2)
+        _out.append(_hand)
+        return _out
 
-    @staticmethod
-    def decode_from_tuple(observation_state):
-        small_blind = (observation_state[2] == 1)
-        card1 = Card.Card.decode(observation_state[0])
-        card2 = Card.Card.decode(observation_state[1])
-        hand = Hand()
-        hand.add_card(card1)
-        hand.add_card(card2)
-        return small_blind, hand
+
+def test_encoder_decoder():
+    """Test encoder and decoder"""
+    hand1 = Hand()
+    initial_chips = 10
+
+    for chips in range(initial_chips * 2):
+        for sb in range(2):
+            for card1_code in range(52):
+                for card2_code in range(52):
+                    if card1_code == card2_code:
+                        continue
+                    # Create hand
+                    card1 = Card.decode(card1_code)
+                    card2 = Card.decode(card2_code)
+                    hand1.clear_hand()
+                    hand1.add_card(card1)
+                    hand1.add_card(card2)
+                    hand1.cards = sorted(hand1.cards, reverse=True)
+                    # Encode and decode
+                    code = PokerEnv.encode(hand1, sb, chips, initial_chips)
+                    [new_chips, new_small_blind, new_hand] = PokerEnv.decode(code)
+                    assert new_chips == int((chips / (2 * initial_chips)) * 4)
+                    assert new_small_blind == sb
+                    assert new_hand.cards[0] == hand1.cards[0]
+                    assert new_hand.cards[1] == hand1.cards[1]
+
+
+if __name__ == "__main__":
+    # test_encoder_decoder()
+    pass
