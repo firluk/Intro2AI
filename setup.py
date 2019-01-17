73fa775d98b0baf64ad99ee260a26501d5f3a243
firluk <firluk@gmail.com>
2019-01-17 23:57:36 +0200

    install requirement - h5py

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='gym_poker',
 
 setup(name='gym_poker',
       version='0.0.1',
-      install_requires=['gym', 'numpy', 'prettytable', 'tensorflow', 'keras==2.1.6']
+      install_requires=['gym', 'numpy', 'prettytable', 'tensorflow', 'keras==2.1.6', 'h5py']
       )
